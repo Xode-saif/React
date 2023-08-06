486697340cd2ee0393e63c1afd51c4c608ec8916
Md saif Ali <saif@Mds-MacBook-Air.local>
2023-08-07 02:01:29 +0530

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import React from 'react'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import Notes from './component/Notes'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Notes/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
